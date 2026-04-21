733d0ea3fabf2f1eaa8d5b39957d0dbc00e7af11
dheeerajkashyap <133700803+dheeerajkashyap@users.noreply.github.com>
2026-04-21 23:17:10 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -196,7 +196,7 @@ React.useEffect(() => {
 
               {/* 2+1 Layout - First two side by side, third full width below */}
 
-              <section className="py-20 bg-background">
+             
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
               <motion.div
                 initial={{ opacity: 0, y: 20 }}
@@ -210,7 +210,7 @@ React.useEffect(() => {
 </h1>
               </motion.div>
             </div>
-              </section>
+              
     
               
   <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8">
